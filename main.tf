8743834b1f0783be61c4b8044669386de0fe5b34
pbudage <113903158+pbudage@users.noreply.github.com>
2022-10-12 16:39:36 +0530

    create vpc and igw

---
+++ main.tf
@@ -0,0 +1,18 @@
+provider "aws" {
+  region = "us-east-1"
+}
+
+resource "aws_internet_gateway" "gw" {
+  tags = {
+    Name = "test-igw"
+  }
+}
+
+resource "aws_vpc" "test-vpc" {
+  cidr_block       = "10.0.0.0/16"
+  instance_tenancy = "default"
+
+  tags = {
+    Name = "test-vpc-drift"
+  }
+}
