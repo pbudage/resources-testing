addd4177b5013a63da7b831a3590c26bdc4b1a73
pbudage <113903158+pbudage@users.noreply.github.com>
2022-10-12 17:55:21 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,17 +2,17 @@ provider "aws" {
   region = "us-east-1"
 }
 
-resource "aws_internet_gateway" "test-iac-gateway" {
+resource "aws_internet_gateway" "test-igw-iac" {
   tags = {
     Name = "test-iac-igw"
   }
 }
 
-resource "aws_vpc" "test-vpc" {
+resource "aws_vpc" "test-vpc-iac" {
   cidr_block       = "10.0.0.0/16"
   instance_tenancy = "default"
 
   tags = {
-    Name = "test-vpc-iac"
+    Name = "test-iac-vpc"
   }
 }
